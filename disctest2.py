f86743b41763e2025ec83215f877289c2b62ad64
MacBook <macbook@Yunas-MacBook-516.local>
2022-02-05 04:54:04 +0000

    got rid of for

--- disctest2.py
+++ disctest2.py
@@ -45,9 +45,9 @@ while True:
          print(x_coord,y_coord)
          if len(x_coord)==2 and len(y_coord)==2:
              for y in range(7):
-                x_val = x_coord[0]
-                y_val = y_coord[1]+((y_coord[0]-y_coord[1])/7)*y
-                cv2.circle(frame,(x_val,y_val),7,(255,255,255),-1)
+                    x_val = x_coord[0]
+                    y_val = y_coord[1]+((y_coord[0]-y_coord[1])/7)*y
+                    cv2.circle(frame,(x_val,y_val),7,(255,255,255),-1)
             
  
 

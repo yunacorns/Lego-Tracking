5d4af1864954c593bbf43c150c0a386c167ebabb
MacBook <macbook@Yunas-MacBook-516.local>
2022-02-18 18:09:47 +0000

    added colour track

--- qrcode.py
+++ qrcode.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import imutils
 from pyzbar.pyzbar import decode
 
 cap= cv2.VideoCapture(1)
@@ -8,6 +9,29 @@ cap.set(4,480)
 
 while True:
     _,frame= cap.read()
+    # blurred_frame = cv2.GaussianBlur(frame,(5,5),0)
+
+    # hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+    # lower_red = np.array([161,155,84])
+    # upper_red = np.array([179,255,255])
+    # mask_red = cv2.inRange(hsv,lower_red,upper_red)
+
+    # cnts_red = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts_red = imutils.grab_contours(cnts_red)
+    # font = cv2.FONT_HERSHEY_COMPLEX
+    # for c in cnts_red:
+    #     area = cv2.contourArea(c)
+    #     if area > 500:
+    #         approx = cv2.approxPolyDP(c, 0.009 * cv2.arcLength(c, True), True)
+    #         cv2.drawContours(frame,[approx],-1,(0,255,0), 3)
+    #         M = cv2.moments(c)
+
+    #         cx = int(M["m10"]/ M["m00"])
+    #         cy = int(M["m01"]/ M["m00"])
+
+    #         cv2.circle(frame,(cx,cy),7,(255,255,255),-1)
+    #         cv2.putText(frame,'red x={}, y={}'.format(cx,cy),(cx-20,cy-20),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),3)
     for barcode in decode(frame):
         print(barcode.data)
         myData = barcode.data.decode('utf-8')
@@ -33,8 +57,32 @@ while True:
         
         if first_left is not None and second_right is not None :
             new_frame = frame[first_top:second_bottom,first_left:second_right]
-            cv2.imshow('new',new_frame)
+            blurred_frame = cv2.GaussianBlur(new_frame,(5,5),0)
 
+            hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+            lower_red = np.array([161,155,84])
+            upper_red = np.array([179,255,255])
+            mask_red = cv2.inRange(hsv,lower_red,upper_red)
+
+            cnts_red = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            cnts_red = imutils.grab_contours(cnts_red)
+            font = cv2.FONT_HERSHEY_COMPLEX
+            for c in cnts_red:
+                area = cv2.contourArea(c)
+                if area > 500:
+                    approx = cv2.approxPolyDP(c, 0.009 * cv2.arcLength(c, True), True)
+                    cv2.drawContours(new_frame,[approx],-1,(0,255,0), 3)
+                    M = cv2.moments(c)
+
+                    cx = int(M["m10"]/ M["m00"])
+                    cy = int(M["m01"]/ M["m00"])
+
+                    cv2.circle(new_frame,(cx,cy),7,(255,255,255),-1)
+                    cv2.putText(new_frame,'red x={}, y={}'.format(cx,cy),(cx-20,cy-20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+
+            cv2.imshow('calibrated',new_frame)
+            
 
         
 

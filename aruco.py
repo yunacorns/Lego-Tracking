3dcffad06b95e88798858664f4a11c17c28e5759
yunacorns <98397924+yunacorns@users.noreply.github.com>
2022-03-21 13:14:06 +0000

    changed to midpoint

--- aruco.py
+++ aruco.py
@@ -15,7 +15,7 @@ def main():
     sock.connect((host,port))
     
 
-    cap= cv2.VideoCapture(1)
+    cap= cv2.VideoCapture(0)
     cap.set(3,640)
     cap.set(4,480)
 
@@ -43,57 +43,47 @@ def main():
         
             pos_1 = ids_formatted.index(1)
             pos_2 = ids_formatted.index(2)
-            # get coordinates of top left of 1 and bottom right of 2 
-            coord1 = bbox[pos_1][0][0]
-            coord2 = bbox[pos_2][0][2]
+            # get coordinates of top left and bottom right of 1 and 2 
+            TLcoord1 = bbox[pos_1][0][0]
+            BRcoord1 = bbox[pos_1][0][2]
+            TLcoord2 = bbox[pos_2][0][0]
+            BRcoord2 = bbox[pos_2][0][2]
      
 
-            coord1_x = int(coord1[0])
-            coord2_x = int(coord2[0])
-            coord1_y = int(coord1[1])
-            coord2_y = int(coord2[1])
+            coord1_x = int(TLcoord1[0])+(int(BRcoord1[0])-int(TLcoord1[0]))/2
+            coord1_y = int(TLcoord1[1])+(int(BRcoord1[1])-int(TLcoord1[1]))/2
+            coord2_x = int(TLcoord2[0])+(int(BRcoord2[0])-int(TLcoord2[0]))/2
+            coord2_y = int(TLcoord2[1])+(int(BRcoord2[1])-int(TLcoord2[1]))/2
             # minus in front of y coords bc webcam origin flipped in x axis compared to unity origin
             position1 = [coord1_x-coord1_x,-(coord1_y-coord1_y),0]
             position2 = [coord2_x-coord1_x,-(coord2_y-coord1_y),0]
             posString1 = ','.join(map(str,position1))
             posString2 = ','.join(map(str,position2))
-            posString12 = posString1 + ','+ posString2
-            # print('pos1',posString1)
-            # print('pos2',posString2)
-            # print('pos',posString12)
-            # sock.sendall(posString12.encode("UTF-8"))
 
-
-            # receivedData = sock.recv(1024).decode("UTF-8")
 
             
             # aruco markers 3 and 4 for the joint coordinates
             if [3] in ids:
                 pos_3 = ids_formatted.index(3)
-                coord3 = bbox[pos_3][0][0]
-                print(coord3)
-                new_coord3_x = int(coord3[0])-coord1_x
-                new_coord3_y = int(coord3[1])-coord1_y
-                # newunity_coord3_y = abs(coord2_y-coord1_y)-new_coord3_y
+                TLcoord3 = bbox[pos_3][0][0]
+                BRcoord3 = bbox[pos_3][0][2]
+                # find midpoints
+                new_coord3_x = (int(TLcoord3[0])+(int(BRcoord3[0])-int(TLcoord3[0]))/2)-coord1_x
+                new_coord3_y = (int(TLcoord3[1])+(int(BRcoord3[1])-int(TLcoord3[1]))/2)-coord1_y
                 position3 = [new_coord3_x,-new_coord3_y,0]
                 posString3 = ','.join(map(str,position3))
-                # print(posString3)
                 
 
 
             if [4] in ids:
                 pos_4 = ids_formatted.index(4)
-                coord4 = bbox[pos_4][0][0]
-                print(coord4)
-                new_coord4_x = int(coord4[0])-coord1_x 
-                new_coord4_y = int(coord4[1])-coord1_y
-                # newunity_coord4_y = abs(coord2_y-coord1_y)-new_coord4_y
+                TLcoord4 = bbox[pos_4][0][0]
+                BRcoord4 = bbox[pos_4][0][2]
+                # find midpoints
+                new_coord4_x = (int(TLcoord4[0])+(int(BRcoord4[0])-int(TLcoord4[0]))/2)-coord1_x 
+                new_coord4_y = (int(TLcoord2[1])+(int(BRcoord2[1])-int(TLcoord2[1]))/2)-coord1_y
                 position4 = [new_coord4_x,-new_coord4_y,0]
                 posString4 = ','.join(map(str,position4))
-                # print(posString4)
-
-                # sock.sendall(posString4.encode("UTF-8"))
-                # receivedData = sock.recv(1024).decode("UTF-8")
 
 
             # use 3 and 4 to find length
@@ -101,7 +91,7 @@ def main():
                 length3_4 = math.sqrt(math.pow(new_coord4_x-new_coord3_x,2)+math.pow(new_coord4_y-new_coord3_y,2))
                 print(length3_4)
                 
-            
+            # send data to unity
             if [1] and [2] and [3] and [4] in ids:
                 posString1234 = posString1 +','+ posString2 + ','+ posString3 + ','+ posString4
                 sock.sendall(posString1234.encode("UTF-8"))
@@ -111,10 +101,10 @@ def main():
 
         
 
-            cv2.circle(frame,(coord1_x,coord1_y),7,(255,255,255),-1)
+            #cv2.circle(frame,(coord1_x,coord1_y),7,(255,255,255),-1)
 
 
-            new_frame = frame[coord1_y:coord2_y,coord1_x:coord2_x]
+            new_frame = frame[int(coord1_y):int(coord2_y),int(coord1_x):int(coord2_x)]
             cv2.imshow('calibrated',new_frame)
 
 
